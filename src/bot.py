f7aa18848e7fcd69e0ccad547d9c7cbd98083176
Ryan Murfitt <rmurfitt@gmail.com>
2014-02-26 22:20:39 +1000

    Add giphy support

--- src/bot.py
+++ src/bot.py
@@ -91,6 +91,10 @@ class Bot():
                 'action': self.__cmdXKCD,
                 'help': 'Get random xkcd comics'
             },
+            '/gif': {
+                'action': self.__cmdRandomGIF,
+                'help': 'Get a random gif, with a optional search term (/gif keyboard cat)'
+            },
             '/?': {
                 'action': self.__cmdAsk,
                 'help': 'Ask me a question'
@@ -269,6 +273,20 @@ class Bot():
 
         else:
             room.speak("Can't connect to xkcd API =(")
+
+    def __cmdRandomGIF(self, room, message):
+        search_values = message.split('/gif', 1)
+        tags = ''
+        if len(search_values) == 2:
+            tags = '+'.join(search_values[1].split())
+        r = requests.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + tags)
+
+        if r.status_code == 200:
+            response = r.json()
+            room.speak(response['data']['image_url'])
+
+        else:
+            room.speak("Can't connect to giphy API =(")
 
     def joinRooms(self, rooms):
         self.rooms = rooms.split(',')
